d400da97210620c6a70100698ee9eebf6fda2266
iced377 <izmerly@gmail.com>
2026-01-24 22:33:57 +0300

    chore: point capacitor to production server (mygutcheck.app)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.iced377.guthealth',
   appName: 'GutHealth',
   webDir: 'out',
-
+  server: {
+    url: 'https://mygutcheck.app',
+    cleartext: false
+  },
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: false,
